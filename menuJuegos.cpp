334551c666a5539b4f0b9b21ce6352362df1020d
edwinpaye <baratheonts@gmail.com>
2018-09-21 20:59:59 -0400

    practica menu de juegos

---
+++ menuJuegos.cpp
@@ -0,0 +1,166 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+//Paye Villca Edwin.
+using namespace std;
+
+int aleatorio(int limite_inferior, int limite_superior){
+    return limite_inferior + rand() % (limite_superior +1 - limite_inferior);
+}
+
+void ejercicio(){
+    int dado, n;
+    cout << "Ingrese cantidad de lanzamientos: ";
+    cin>>n;
+    srand(time(NULL));
+    for(int c=1;c<=n;c++){
+        dado = aleatorio(1, 6);
+        cout<<dado<<'\t';
+    }
+}
+
+/*realizar un algoritmo para realizar un juego de
+lanzamiento de un dado. El juego termina cuando
+el valor del dado obtenido es un numero par, mostrar
+por pantalla el valor del dado y el numero de intentos
+en el que obtuvo el resultado.*/
+
+void ejercicioUno(){
+    srand(time(NULL));
+    int dado=1, contador=0;
+    while(dado%2!=0){
+        dado = aleatorio(1, 6);
+        contador++;
+        if(dado%2!=0){
+            cout<<dado<<endl;
+        }
+    }
+    cout<<dado<<endl;
+    cout<<"El dado es: "<<dado<<", con "<<contador<<" intento(s)."<<endl;
+}
+
+void ejercicioDos(){
+    srand(time(NULL));
+    int dadoA, dadoB, dadoC, contador=0;
+    bool validador = true;
+    while(validador){
+        dadoA = aleatorio(1, 6);
+        dadoB = aleatorio(1, 6);
+        dadoC = aleatorio(1, 6);
+        contador++;
+        if(dadoA==dadoB&&dadoA==dadoC){
+            cout<<dadoA<<" - "<<dadoB<<" - "<<dadoC<<endl;
+            validador = false;
+        }else{
+            cout<<dadoA<<" - "<<dadoB<<" - "<<dadoC<<endl;
+        }
+    }
+    cout<<"Trica en el turno: "<<contador<<endl;
+}
+
+/*lanzar tres cartas, el primero y segundo de 1 al 14 el tercero de 0 a 14
+el juego termina cuando se forma una trica, considerar a la tercera carta como
+comodin cuando obtenga el valor de 0*/
+
+void ejercicioTres(){
+    srand(time(NULL));
+    int cartA, cartB, cartC, contador=0;
+    bool validador = true;
+    while(validador){
+        cartA = aleatorio(1, 6);
+        cartB = aleatorio(1, 6);
+        cartC = aleatorio(0, 6);
+        contador++;
+        if(cartA==cartB&&(cartC==0||cartA==cartC)){
+            cout<<cartA<<" - "<<cartB<<" - "<<cartC<<endl;
+            validador = false;
+        }else{
+            cout<<cartA<<" - "<<cartB<<" - "<<cartC<<endl;
+        }
+    }
+    if(cartC==0){
+        cout<<"Par y comodin en el turno: "<<contador<<endl;
+    }else{
+        cout<<"Trica en el turno: "<<contador<<endl;
+    }
+}
+
+bool auxiliar(int intento, string jugador){
+    int cartA, cartB, cartC;
+        cartA = aleatorio(1, 6);
+        cartB = aleatorio(1, 6);
+        cartC = aleatorio(0, 6);
+        cout<<intento<<". "<<jugador<<": "<<cartA<<" - "<<cartB<<" - "<<cartC<<endl;
+        if(cartA==cartB&&(cartC==0||cartA==cartC)){
+            return false;
+        }
+    return true;
+}
+
+void ejercicioCuatro(){
+    srand(time(NULL));
+    int contadorA=0, contadorB=0;
+    bool jugadorA = true, jugadorB = true;
+    while(jugadorA||jugadorB){
+        if(jugadorA){
+            contadorA++;
+            jugadorA = auxiliar(contadorA, "Jugador Uno");
+        }
+        if(jugadorB){
+            contadorB++;
+            jugadorB = auxiliar(contadorB, "Jugador Dos");
+        }
+    }
+    if(contadorA==contadorB){
+        cout<<"Empate en el turno "<<contadorA<<endl;
+    }else{
+        if(contadorA<contadorB){
+            cout<<"Gana el jugador Uno con "<<contadorA<<" intentos."<<endl;
+            cout<<"El jugador Dos con "<<contadorB<<" intentos."<<endl;
+        }else{
+            cout<<"Gana el jugador Dos con "<<contadorB<<" intentos."<<endl;
+            cout<<"El jugador Uno con "<<contadorA<<" intentos."<<endl;
+        }
+    }
+}
+
+void ejmSwitch(){
+    int x;
+    cout<<"Elegir opcion: ";
+    cin>>x;
+    switch(x){
+        case 1: cout<<"Lunes"<<endl; break;
+        case 2: cout<<"Martes"<<endl; break;
+        case 3: cout<<"Miercoles"<<endl; break;
+        case 4: cout<<"Jueves"<<endl; break;
+        case 5: cout<<"Viernes"<<endl; break;
+        case 6: cout<<"Sabado"<<endl; break;
+        case 7: cout<<"Domingo"<<endl; break;
+        default: cout<<"Opcion fuera de rango"<<endl; break;
+    }
+}
+//Menu de juegos
+void menuJuegos(){
+    int x;
+    do{
+        cout<<"---* MENU JUEGOS *---"<<endl;
+        cout<<"1. Juego Uno."<<endl;
+        cout<<"2. Juego Dos."<<endl;
+        cout<<"3. Salir."<<endl;
+        cout<<"Elejir opcion: ";
+        cin>>x;
+        switch(x){
+            case 1: ejercicioTres(); break;
+            case 2: ejercicioCuatro(); break;
+            case 3: cout<<"Hasta luego..."<<endl; break;
+            default: cout<<"Opcion fuera de rango"<<endl; break;
+        }
+    }while(x!=3);
+}
+
+int main()
+{
+    menuJuegos();
+    return 0;
+}
