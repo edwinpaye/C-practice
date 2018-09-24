8eab1e4890762a56e6f87f00787a19822d5a6a40
edwinpaye <baratheonts@gmail.com>
2018-09-24 17:42:31 -0400

    add method ejemUno

--- ciclos.cpp
+++ ciclos.cpp
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
 void ejemUno(){
-	
+	int alumnos, auxiliar=0, nota=0;
+	printf("Ingrese la cantidad de alumnos: ");
+	scanf("%d", &alumnos);
+	for (int i = 1; i <= alumnos; ++i)
+	{
+		for (int j = 1; j <= 5; ++j)
+		{
+			printf("Ingrese la nota del alumno %d\n de la unidad %d\n ", i, j);
+			scanf("%d", &nota);
+			auxiliar += nota;
+		}
+
+	}
+	printf("%d\n", alumnos);
 }
 
 main(){
-
+	ejemUno();
 }
