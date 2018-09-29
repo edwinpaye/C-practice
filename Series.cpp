25f252aba71ddaf98da7ddbeaab6ad13a0c1d4fa
edwinpaye <baratheonts@gmail.com>
2018-09-29 00:11:41 -0400

    add method ejemDos

--- Series.cpp
+++ Series.cpp
@@ -14,6 +14,21 @@ void ejemUno(){
 	printf("Cantidad de numeros par: %d\n", cont);
 }
 
+void ejemDos(){
+	int num, contPares=0, contImpares=0;
+	printf("Ingrese un numero entero positivo: ");
+	scanf("%d", &num);
+	while(num!=0){
+		if ((num%10)%2==0){
+			contPares++;
+		}else{
+			contImpares++;
+		}
+		num = num/10;
+	}
+	printf("Cantidad de numeros pares: %d y numeros impares: %d\n", contPares, contImpares);
+}
+
 main(){
 	menu();
 }
