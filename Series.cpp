3c78d34c8792a3e58c6a3ede3dcfde834d3297dc
edwinpaye <baratheonts@gmail.com>
2018-09-29 00:11:15 -0400

    add method ejemUno

---
+++ Series.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+void ejemUno(){
+	int num, cont=0;
+	printf("Ingrese un numero entero positivo: ");
+	scanf("%d", &num);
+	while(num!=0){
+		if ((num%10)%2==0){
+			cont++;
+		}
+		num = num/10;
+	}
+	printf("Cantidad de numeros par: %d\n", cont);
+}
+
+main(){
+	menu();
+}
