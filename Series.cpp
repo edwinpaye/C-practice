cd10a26cb8b7f7db53281b2e906a94050234e4a4
edwinpaye <baratheonts@gmail.com>
2018-09-29 00:12:10 -0400

    add method ejemTres

--- Series.cpp
+++ Series.cpp
@@ -29,6 +29,28 @@ void ejemDos(){
 	printf("Cantidad de numeros pares: %d y numeros impares: %d\n", contPares, contImpares);
 }
 
+void ejemTres(){
+	int num, contPares=0, contImpares=0;
+	printf("Ingrese un numero entero positivo de un digito: ");
+	scanf("%d", &num);
+	if (num<10&&num>-1){
+		switch(num){
+			case 0: printf("cero\n"); break;
+			case 1: printf("uno\n"); break;
+			case 2: printf("dos\n"); break;
+			case 3: printf("tres\n"); break;
+			case 4: printf("cuatro\n"); break;
+			case 5: printf("cinco\n"); break;
+			case 6: printf("seis\n"); break;
+			case 7: printf("siete\n"); break;
+			case 8: printf("ocho\n"); break;
+			case 9: printf("nueve\n"); break;
+		}
+	}else{
+		printf("Error de ingreso\n");
+	}
+}
+
 main(){
 	menu();
 }
