7de5e0a8c2007ca3b1c589c04bc33137a4e0e83f
edwinpaye <baratheonts@gmail.com>
2018-10-02 16:12:50 -0400

    add method ejerUno

--- Examen.cpp
+++ Examen.cpp
@@ -19,6 +19,36 @@ int auxDos(int x, int y){
         return x;
 }
 
+void ejerUno(){
+    int x, y, z, result;
+    printf("\nIngrese tres numeros diferentes\n");
+    printf("Ingrese el primer numero entero: ");
+    scanf("%d", &x);
+    printf("Ingrese el segundo numero entero: ");
+    scanf("%d", &y);
+    printf("Ingrese el tercer numero entero: ");
+    scanf("%d", &z);
+    if(auxUno(auxUno(x,y),z)==x){
+        if(auxDos(auxDos(x,y),z)==y)
+            result = z;
+        else
+            result = y;
+    }else{
+        if(auxUno(auxUno(x,y),z)==y){
+            if(auxDos(auxDos(x,y),z)==x)
+                result = z;
+            else
+                result = x;
+        }else{
+            if(auxDos(auxDos(x,y),z)==x)
+                result = y;
+            else
+                result = x;
+        }
+    }
+    printf("El numero medio es: %d\n", result);
+}
+
 main(){
     menu();
 }
