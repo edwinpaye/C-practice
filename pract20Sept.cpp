94a41db67df103c95cf2323ba0fef74095746dfc
edwinpaye <baratheonts@gmail.com>
2018-10-05 02:54:18 -0400

    add method ejercicioUno

--- pract20Sept.cpp
+++ pract20Sept.cpp
@@ -19,6 +19,25 @@ void ejercicio(){
     }
 }
 
+/*realizar un algoritmo para realizar un juego de
+lanzamiento de un dado. El juego termina cuando
+el valor del dado obtenido es un numero par, mostrar
+por pantalla el valor del dado y el numero de intentos
+en el que obtuvo el resultado.*/
+
+void ejercicioUno(){
+    srand(time(NULL));
+    int dado=1, contador=0;
+    while(dado%2!=0){
+        dado = aleatorio(1, 6);
+        contador++;
+        if(dado%2!=0)
+            cout<<dado<<endl;
+    }
+    cout<<dado<<endl;
+    cout<<"El dado es: "<<dado<<", con "<<contador<<" intento(s)."<<endl;
+}
+
 main(){
     menuJuegos();
 }
