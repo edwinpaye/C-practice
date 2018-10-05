388f700188c113c39d2287214f2ab784ad73f7ce
edwinpaye <baratheonts@gmail.com>
2018-10-05 02:53:24 -0400

    add method aleatorio

--- pract20Sept.cpp
+++ pract20Sept.cpp
@@ -4,6 +4,10 @@
 
 using namespace std;
 
+int aleatorio(int limite_inferior, int limite_superior){
+    return limite_inferior + rand() % (limite_superior +1 - limite_inferior);
+}
+
 main(){
     menuJuegos();
 }
