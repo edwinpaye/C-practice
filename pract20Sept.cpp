7517e9a2fd07515d8495e7ef5d33a556edb6e905
edwinpaye <baratheonts@gmail.com>
2018-10-05 02:53:57 -0400

    add method ejercicio

--- pract20Sept.cpp
+++ pract20Sept.cpp
@@ -8,6 +8,17 @@ int aleatorio(int limite_inferior, int limite_superior){
     return limite_inferior + rand() % (limite_superior +1 - limite_inferior);
 }
 
+void ejercicio(){
+    int dado, n;
+    cout << "Ingrese cantidad de lanzamientos: ";
+    cin>>n;
+    srand(time(NULL));
+    for(int c=1;c<=n;c++){
+        dado = aleatorio(1, 6);
+        cout<<dado<<'\t';
+    }
+}
+
 main(){
     menuJuegos();
 }
